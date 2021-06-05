2771b3b0ec32a345b7283365b5a76612282b1742
Koby <koby412@hotmail.com.tw>
2021-06-05 18:15:15 +0800

    完成新增、刪除、修改、讀取(CRUD)

---
+++ slnCRUD/prjCRUD/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//加入要引用的模型
+using prjCRUD.Models; 
+
+namespace prjCRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        CompanyEntities db = new CompanyEntities();
+
+        //將讀取出來的資料顯示在頁面上
+        public ActionResult Index()
+        {
+            var emps = db.Employee.OrderByDescending(m => m.fId).ToList();
+            return View(emps);
+        }
+
+        //新增
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string fName,string fPhone,int fSalary)
+        {
+            Employee emp = new Employee();
+            emp.fName = fName;
+            emp.fPhone = fPhone;
+            emp.fSalary = fSalary;
+            db.Employee.Add(emp);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //編輯
+        public ActionResult Update(int fId)
+        {
+            //找到指定的id
+            var emp = db.Employee.Where(m => m.fId == fId).FirstOrDefault();
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult Update(int fId,string fName,string fPhone,int fSalary)
+        {
+            var emp = db.Employee.Where(m => m.fId == fId).FirstOrDefault();
+            emp.fName = fName;
+            emp.fPhone = fPhone;
+            emp.fSalary = fSalary;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //刪除
+        public ActionResult Delete(int fId)
+        {
+            var emp = db.Employee.Where(m => m.fId == fId).FirstOrDefault();
+            db.Employee.Remove(emp);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
